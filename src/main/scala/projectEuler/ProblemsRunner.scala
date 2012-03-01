b872a707f13e6f747c85a1597f3d343f305bfd64
Robert Giacinto <robert.giacinto@gmail.com>
2012-03-01 20:31:54 +0100

    problem 2 solved

---
+++ src/main/scala/projectEuler/ProblemsRunner.scala
@@ -0,0 +1,11 @@
+package projectEuler
+
+import problems.{Problem1, Problem2}
+
+/**
+ * Problems from www.projecteuler.net
+ */
+object ProblemsRunner extends App{
+  Problem1.solve
+  Problem2.solve
+}
--- src/main/scala/projectEuler/problems/Problem1.scala
+++ src/main/scala/projectEuler/problems/Problem1.scala
@@ -1,4 +1,4 @@
-package projectEuler
+package projectEuler.problems
 
 /**
  * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -16,7 +16,8 @@ object Problem1 {
   }
 
 
-  def solve = {
+  def solve {
+    println("=========================================================================================================")
     println("Problem 1: \n" +
       "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.\n  " +
       "Find the sum of all the multiples of 3 or 5 below 1000.\n")
@@ -25,5 +26,6 @@ object Problem1 {
     val sum15 = sum(sumDivisibleByN(1000, 15))
     print("Solution: ")
     println(sum3 + sum5 - sum15)
+    println("=========================================================================================================")
   }
 }
